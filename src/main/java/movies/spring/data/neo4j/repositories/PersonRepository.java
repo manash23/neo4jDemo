48024d92980de6161337af4f4f21a5f48b8d5b07
Manash <manashmahanta23@gmail.com>
2018-11-05 09:32:38 +0530

    a small neo4j demo application

---
+++ src/main/java/movies/spring/data/neo4j/repositories/PersonRepository.java
@@ -0,0 +1,14 @@
+package movies.spring.data.neo4j.repositories;
+
+import movies.spring.data.neo4j.domain.Person;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ */
+public interface PersonRepository extends Neo4jRepository<Person, Long> {
+
+    Person findByName(String name);
+
+}
